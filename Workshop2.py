a0edc37b825a4d36fa9b55c519e4fab208b1f7e4
antondubov <antondubov1@yandex.ru>
2022-10-12 00:05:25 +0300

    Вычисение суммы по формуле

--- Workshop2.py
+++ Workshop2.py
@@ -17,12 +17,26 @@
 # Пример:
 # - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
+# number = int(input('Введите число: '))
+# mul = 1
+# factorial_list = []
+# for i in range(1, number+1):
+#     mul *= i
+#     factorial_list.append(mul)
+# print(factorial_list, sep=',')
+
+#-----------------------------------------Семинар 2 задание 3-----------------------------------------#
+# Задайте список из n чисел последовательности (1 + 1/n)^n и выведите на экран их сумму.
+# Пример:
+# - Для n = 4: {1: 2, 2: 2.25, 3: 2.37, 4: 2.44}
+
 number = int(input('Введите число: '))
-mul = 1
-factorial_list = []
+result = 0
+sum_value = 0
+output_list = []
 for i in range(1, number+1):
-    mul *= i
-    factorial_list.append(mul)
-print(factorial_list, sep=',')
-
-
+    result = (1 + (1 / i)) ** i
+    sum_value += result
+    output_list.append(f'{i}: {round(result,2)}')
+print(output_list, sep=',')
+print(f'Сумма последовательности чисел равна = {round(sum_value,2)}')
