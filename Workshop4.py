5a886b4cb8e09583f7b7ddf072586a5bba453541
antondubov <antondubov1@yandex.ru>
2022-10-23 14:08:08 +0300

    Формирование случайного многочлена

--- Workshop4.py
+++ Workshop4.py
@@ -21,23 +21,37 @@
 # print(f'Список простых делителей {lst}')
 #-----------------------------------------Семинар 4 задание 3-----------------------------------------#
 # Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
-lst = [1,2,2,3,5,6,7,7,5,9]
-idx = 0
-out_lst = []
-while idx < len(lst):
-    counter = 0
-    for i in lst:
-        if lst[idx] == i:
-            counter +=1
-    if counter < 2:
-        out_lst.append(lst[idx])
-    idx += 1
-
-print(f'Уникальные элементы {out_lst}')
+# lst = [1,2,2,3,5,6,7,7,5,9]
+# idx = 0
+# out_lst = []
+# while idx < len(lst):
+#     counter = 0
+#     for i in lst:
+#         if lst[idx] == i:
+#             counter +=1
+#     if counter < 2:
+#         out_lst.append(lst[idx])
+#     idx += 1
+#
+# print(f'Уникальные элементы {out_lst}')
 #-----------------------------------------Семинар 4 задание 4-----------------------------------------#
 # Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
 # Пример:
 # - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+from random import randint
+from sympy import symbols
+from math import prod
+
+max_val = 100
+k = int(input('Введите натуральную степень k:'))
+
+koeff = [randint(-max_val, max_val) for i in range(k)] + [randint(1, max_val)]
+x = symbols('x')
+
+print(sum(map(prod, zip(koeff, [ x**i for i in range(k + 1)]))), '= 0')
+
+with open('file.txt','w', encoding='utf-8') as file:
+    file.write(str(sum(map(prod, zip(koeff, [x ** i for i in range(k + 1)])))) + '= 0')
 
 #-----------------------------------------Семинар 4 задание 5-----------------------------------------#
 # Даны два файла, в каждом из которых находится запись многочлена. Задача - сформировать файл, содержащий сумму многочленов.
