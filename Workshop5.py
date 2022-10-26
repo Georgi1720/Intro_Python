4b4800f98cb27457e232e223675731d00e35337e
antondubov <antondubov1@yandex.ru>
2022-10-26 20:36:06 +0300

    RLE сжатие

--- Workshop5.py
+++ Workshop5.py
@@ -15,43 +15,77 @@ print(text_without_word)
 # a) Добавьте игру против бота
 # b) Подумайте как наделить бота ""интеллектом""
 
-import random as r
-player_1 = input("Введите имя первого игрока: ")
-player_2 = input("Введите имя второго игрока: ")
-queue_turn = r.randint(1,3)
-total_candys = 121
-candys_in_turn = 28
-if queue_turn == 1: print(f'Первый ход делает {player_1}. Нельзя брать больше 28 конфет!')
-else: print(f'Первый ход делает {player_2}. Нельзя брать больше 28 конфет!')
-
-while total_candys > 0:
-    if queue_turn == 1:
-        candys_in_turn = int(input(f'Осталось {total_candys} конфет. {player_1}, сколько конфет ты возьмешь? :'))
-        if candys_in_turn <= 28:
-            total_candys -= candys_in_turn
-            queue_turn = 2
-        else:
-            print(f'{player_1}, можно брать не больше 28 конфет!')
-            candys_in_turn = 1
-    if queue_turn == 2:
-        candys_in_turn = int(input(f'Осталось {total_candys} конфет. {player_2}, сколько конфет ты возьмешь? :'))
-        if candys_in_turn <= 28:
-            total_candys -= candys_in_turn
-            queue_turn = 1
-        else:
-            print(f'{player_1}, можно брать не больше 28 конфет!')
-            candys_in_turn = 1
-    if total_candys <= 28:
-        if queue_turn == 1:
-            print(f'Осталось {total_candys} конфет. {player_1} забирает их и выигрывает!')
-            break
-        else:
-            print(f'Осталось {total_candys} конфет. {player_2} забирает их и выигрывает!')
-            break
+# import random as r
+# player_1 = input("Введите имя первого игрока: ")
+# player_2 = input("Введите имя второго игрока: ")
+# queue_turn = r.randint(1,3)
+# total_candys = 121
+# candys_in_turn = 28
+# if queue_turn == 1: print(f'Первый ход делает {player_1}. Нельзя брать больше 28 конфет!')
+# else: print(f'Первый ход делает {player_2}. Нельзя брать больше 28 конфет!')
+#
+# while total_candys > 0:
+#     if queue_turn == 1:
+#         candys_in_turn = int(input(f'Осталось {total_candys} конфет. {player_1}, сколько конфет ты возьмешь? :'))
+#         if candys_in_turn <= 28:
+#             total_candys -= candys_in_turn
+#             queue_turn = 2
+#         else:
+#             print(f'{player_1}, можно брать не больше 28 конфет!')
+#             candys_in_turn = 1
+#     if queue_turn == 2:
+#         candys_in_turn = int(input(f'Осталось {total_candys} конфет. {player_2}, сколько конфет ты возьмешь? :'))
+#         if candys_in_turn <= 28:
+#             total_candys -= candys_in_turn
+#             queue_turn = 1
+#         else:
+#             print(f'{player_1}, можно брать не больше 28 конфет!')
+#             candys_in_turn = 1
+#     if total_candys <= 28:
+#         if queue_turn == 1:
+#             print(f'Осталось {total_candys} конфет. {player_1} забирает их и выигрывает!')
+#             break
+#         else:
+#             print(f'Осталось {total_candys} конфет. {player_2} забирает их и выигрывает!')
+#             break
 
 #-----------------------------------------Семинар 5 задание 3-----------------------------------------#
 # Создайте программу для игры в ""Крестики-нолики"".
 
+#       0_o
+
 #-----------------------------------------Семинар 5 задание 4-----------------------------------------#
 # Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
 # Входные и выходные данные хранятся в отдельных текстовых файлах.
+
+with open('text_words.txt', 'w', encoding='UTF-8') as file:
+    file.write(input('Напишите текст необходимый для сжатия: '))
+with open('text_words.txt', 'r') as file:
+    my_txt = file.readline()
+    txt_compr = my_txt.split()
+
+print(my_txt)
+
+def file_encod(txt):
+    encond = ''
+    prev_char = ''
+    count = 1
+    if not txt:
+        return ''
+
+    for char in txt:
+        if char != prev_char:
+            if prev_char:
+                encond += str(count) + prev_char
+            count = 1
+            prev_char = char
+        else:
+            count += 1
+    else:
+        encond += str(count) + prev_char
+        return encond
+
+txt_compr = file_encod(my_txt)
+with open('text_code_words.txt', 'w', encoding='UTF-8') as file:
+    file.write(f'{txt_compr}')
+print(txt_compr)
